7531543cfe95b26ee5027ecf4d6f386f0df087e9
Mars <marosakraid@gmail.com>
2025-05-23 21:55:01 +0500

    Добавлены тесты

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,79 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+        // максимальное число не первое по счету
+    void findMaxNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+        // максимальное число повторяется
+    void findMaxDuble() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+        // отрицательные числа
+    void findMaxNegative() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -11, -12};
+        long expected = -3;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+        // один элемент
+    void findMaxSingle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+        // одинаковое максимальное число
+    void findMaxOnlyMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 12, 12, 12, 12, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
+// -------------------------------------------------------
+//[INFO]  T E S T S
+//[INFO] -------------------------------------------------------
+//[INFO] Running ru.netology.statistic.StatisticsServiceTest
+//[INFO] Tests run: 6, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.038 s - in ru.netology.statistic.StatisticsServiceTest
+//[INFO]
+//[INFO] Results:
+//[INFO]
+//[INFO] Tests run: 6, Failures: 0, Errors: 0, Skipped: 0
